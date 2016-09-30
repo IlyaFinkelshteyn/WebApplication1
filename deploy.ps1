2fa816914839cb334aecca25157fe47e72c27f3b
IlyaFinkelshteyn <ilyaf@appveyor.com>
2016-09-30 15:30:35 -0700

    Delete unneeded

--- deploy.ps1
+++
@@ -1,5 +0,0 @@
-Write-host 'Setting application pool to work with ASP.NET core'
-import-module WebAdministration
-$appPool = Get-Item IIS:\AppPools\$env:APPLICATION_SITE_NAME
-$appPool.managedRuntimeVersion = ""
-$appPool | set-item
