e37965c1c47e713077a51c6c0a3e628a551b7510
Ilya Finkelshteyn <ilyaf@appveyor.com>
2017-09-29 18:21:39 -0700

    Update deploy.ps1

--- WebApplication1/deploy.ps1
+++ WebApplication1/deploy.ps1
@@ -1,6 +1,2 @@
 Write-host "APPLICATION_SITE_NAME: $env:APPLICATION_SITE_NAME"
-#Write-host 'Setting application pool to work with ASP.NET core'
-#import-module WebAdministration
-#$appPool = Get-Item IIS:\AppPools\$env:APPLICATION_SITE_NAME
-#$appPool.managedRuntimeVersion = ""
-#$appPool | set-item
+Write-host "ARTIFACT_FILENAME: $env:ARTIFACT_FILENAME"
